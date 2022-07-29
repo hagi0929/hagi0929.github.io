3e3394669f95ab78b80e9af161dc6655392fbad8
pc <ministove3yo@gmail.com>
2022-07-29 15:34:02 +0900

    ㅇd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useRef } from "react";
+import { Navigate, Route } from "react-router-dom";
 
 function App() {
+  const focusHome = useRef(null);
+  function fuck () {
+    focusHome.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+  <Route path={"/test"} render={fuck()}></Route>;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>안녕하세요?</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtu.be/Arr03aiuDB8"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          d 누르지 마세요s
+        </a>
+      </header>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p ref={focusHome}>ㅆㅅㅌㅊ</p>
+        <a
+          className="App-link"
+          href="https://youtu.be/Arr03aiuDB8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          누르지 마세요
+        </a>
+      </header>
+      <header id="test1">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>ㅆㅅㅌㅊ</p>
+        <a
+          className="App-link"
+          href="https://youtu.be/Arr03aiuDB8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          누르지 마세요
+        </a>
+      </header>
+      <header id="test2">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>ㅆㅅㅌㅊ</p>
+        <a
+          className="App-link"
+          href="https://youtu.be/Arr03aiuDB8"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          누르지 마세요
         </a>
       </header>
     </div>
