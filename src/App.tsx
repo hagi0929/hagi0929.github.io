f0a55c7387c2c7a7e0c45747e4207d84793628a8
pc <ministove3yo@gmail.com>
2022-08-05 13:23:40 +0900

    did much things with UI

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,43 @@ let optionList = [
   ["blackFont", 3, " focus"],
 ];
 
+function useWindowDimensions() {
+  const hasWindow = typeof window !== "undefined";
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    if (hasWindow) {
+      const handleResize = () => {
+        setWindowDimensions(getWindowDimensions());
+      };
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [hasWindow]);
+
+  return windowDimensions;
+}
+
 function App() {
+  const { height, width } = useWindowDimensions();
   const pagesObserver = new Array(4);
   const themeChooser = [
     <HomeTheme />,
     <AboutTheme />,
-    <HomeTheme />,
+    <AboutTheme />,
     <AboutTheme />,
   ];
   const pageRef = useMemo(
@@ -44,27 +75,36 @@ function App() {
       });
     }
   };
-  const observerOption = {
-    threshold: 0.02,
-    rootMargin: "10px",
-  };
   const menuClicked = (pageClicked: number) => {
     goToPage(pageClicked);
-    setPage(pageClicked);
   };
   useEffect(() => {
     const temp = new Array(4).fill(false);
     for (let i = 0; i < 4; i += 1) {
+      // @ts-ignore
+      const ratio = height / 2 / pageRef[i].current?.clientHeight -0.01;
+      const observerOption = {
+        rootMargin: "10px",
+        threshold: [ratio,ratio+0.02],
+      };
+      console.log("ratio:", i, observerOption["threshold"])
       pagesObserver[i] = new IntersectionObserver((entries, observer) => {
-        temp[i] = entries[0].isIntersecting;
+        console.log(i, entries[0].intersectionRatio);
+        temp[i] = entries[0].intersectionRatio > ratio;
         if (temp.filter(Boolean).length == 1) {
           setPage(temp.indexOf(true));
         }
       }, observerOption);
       pagesObserver[i].observe(pageRef[i].current);
     }
-  }, []);
-
+  }, [
+    height,
+    width,
+    pageRef[0].current?.clientHeight,
+    pageRef[1].current?.clientHeight,
+    pageRef[2].current?.clientHeight,
+    pageRef[3].current?.clientHeight,
+  ]);
   return (
     <div className="App">
       {themeChooser[page]}
