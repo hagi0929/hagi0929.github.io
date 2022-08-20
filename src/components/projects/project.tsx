4ed9b0c592a7213dd6536bee16e4ba193b1f59b4
pc <ministove3yo@gmail.com>
2022-08-20 22:39:02 +0900

    animation

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -4,24 +4,33 @@ import tech from "../../../assets/techstack.json";
 import awards from "../../../assets/techstack.json";
 import Typewriter from "typewriter-effect";
 
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 function ProjectContent() {
-  return <div className={"projectLayout"}>
-    <div className={"primaryProjectContainer"}>
-
-    </div>
-  </div>;
-}
-
-function Project() {
   return (
-    <div className={"mainLayout"}>
-      <div className={"side "}></div>
-      <ProjectContent />
-      <div className={"side1 "}></div>
+    <div className={"projectLayout"}>
+      <div className={"primaryProjectContainer"}>
+        <div className={"primaryProjectText"}>
+          <span>Projects</span>
+          <span>Throughout 6 years of programming experience I have made planty of projects</span>
+          <span>here are some of the list...</span>
+          <span> {"< >"} </span>
+        </div>
+        <div className={"primaryProjectSlide"}>
+
+        </div>
+      </div>
     </div>
-  );
+);
 }
+
+function Project()
+  {
+    return (
+      <div className={"mainLayout"}>
+        <ProjectContent/>
+      </div>
+    );
+  }
 
 export default Project;
