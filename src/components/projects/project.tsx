c0d9fce28b94ef5aca36f00792458459cab9a525
minis <ministove3yo@gmail.com>
2022-08-21 16:38:26 +0900

    project: added slide and text

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -5,32 +5,52 @@ import awards from "../../../assets/techstack.json";
 import Typewriter from "typewriter-effect";
 
 import {useEffect, useRef, useState} from "react";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Pagination} from "swiper";
 
 function ProjectContent() {
   return (
     <div className={"projectLayout"}>
       <div className={"primaryProjectContainer"}>
         <div className={"primaryProjectText"}>
-          <span>Projects</span>
-          <span>Throughout 6 years of programming experience I have made planty of projects</span>
-          <span>here are some of the list...</span>
-          <span> {"< >"} </span>
+          <span className={"titleStyle"}>Projects</span>
+          <span
+            className={"contentStyle"}>Throughout 6 years of programming experience I have made planty of projects</span>
+          <span className={"subContentStyle"}>here are some of the list...</span>
         </div>
-        <div className={"primaryProjectSlide"}>
+        <div className={"primaryProjectSlideContainer"}>
+          <Swiper
+            className={"primaryProjectSlide"}
+            slidesPerView={"auto"}
+            centeredSlides={true}
+            spaceBetween={30}
+            grabCursor={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+          >
+            <SwiperSlide className={"card"}>
+
+              34
+            </SwiperSlide>
+            <SwiperSlide className={"card"}>
+              34
+            </SwiperSlide>
+          </Swiper>
 
         </div>
       </div>
     </div>
-);
+  );
 }
 
-function Project()
-  {
-    return (
-      <div className={"mainLayout"}>
-        <ProjectContent/>
-      </div>
-    );
-  }
+function Project() {
+  return (
+    <div className={"mainLayout"}>
+      <ProjectContent/>
+    </div>
+  );
+}
 
 export default Project;
