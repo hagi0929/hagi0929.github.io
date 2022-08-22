156a8b2425a57a8f3044b988e1bb37feda42ce74
minis <ministove3yo@gmail.com>
2022-08-22 17:51:18 +0900

    project: added slide and text

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -4,11 +4,37 @@ import tech from "../../../assets/techstack.json";
 import awards from "../../../assets/techstack.json";
 import Typewriter from "typewriter-effect";
 
-import {useEffect, useRef, useState} from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
+import {createRef, useEffect, useMemo, useRef, useState} from "react";
+import {Swiper, SwiperSlide, useSwiper} from "swiper/react";
 import {Pagination} from "swiper";
 
+const project = [{
+  image: "https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images02.jpg",
+  title: "title1"
+
+}]
+
 function ProjectContent() {
+  const [my_swiper, set_my_swiper] = useState(useSwiper());
+  const mainProjectRef = useMemo(
+    () =>
+      Array(project.length)
+        .fill(0)
+        .map((i) => createRef<HTMLInputElement>()),
+    []
+  );
+
+  // @ts-ignore
+  let mainProjectCards = []
+  for (let i in mainProjectRef) {
+    console.log(project[i])
+    const title = project[i]["title"]
+    mainProjectCards.push(
+      <SwiperSlide className={"card"}>
+        <div ref={mainProjectRef[i]}>{title}</div>
+      </SwiperSlide>
+    )
+  }
   return (
     <div className={"projectLayout"}>
       <div className={"primaryProjectContainer"}>
@@ -17,26 +43,27 @@ function ProjectContent() {
           <span
             className={"contentStyle"}>Throughout 6 years of programming experience I have made planty of projects</span>
           <span className={"subContentStyle"}>here are some of the list...</span>
+          <div className={"control"}>
+            <div className={"controlButton"} onClick={() => my_swiper.slidePrev()}>{"<"}</div>
+            <div className={"controlButton"} onClick={() => my_swiper.slideNext()}>{">"}</div>
+          </div>
         </div>
         <div className={"primaryProjectSlideContainer"}>
           <Swiper
+            onInit={(ev) => {
+              set_my_swiper(ev)
+            }}
             className={"primaryProjectSlide"}
-            slidesPerView={"auto"}
+            slidesPerView={1}
             centeredSlides={true}
-            spaceBetween={30}
+            spaceBetween={20}
             grabCursor={true}
             pagination={{
               clickable: true,
             }}
             modules={[Pagination]}
           >
-            <SwiperSlide className={"card"}>
-
-              34
-            </SwiperSlide>
-            <SwiperSlide className={"card"}>
-              34
-            </SwiperSlide>
+            {mainProjectCards}
           </Swiper>
 
         </div>
